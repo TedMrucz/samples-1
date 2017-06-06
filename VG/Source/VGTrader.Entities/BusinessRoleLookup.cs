c60c49357a0f36ae64fe387b382faa63f1776d67
TedMruczkowski <TedMruczkowski@users.noreply.github.com>
2017-06-06 18:22:11 -0400

    Add files via upload

---
+++ VG/Source/VGTrader.Entities/BusinessRoleLookup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VGTrader.Entities
+{
+    public class BusinessRoleLookup
+    {
+		public BusinessRoleLookup(int id, string shortDesc)
+		{
+			Id = id;
+			ShortDesc = shortDesc;
+		}
+		public int Id { set; get; }
+		public string ShortDesc { set; get; }
+	}
+}
---
+++ VG/Source/VGTrader.Entities/CurrentUserParam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VGTrader.Entities
+{
+	public class CurrentUserParam
+	{
+		public CurrentUserParam(string loginName, int userRoleLevel, string userRole)
+		{
+			this.LoginName = loginName;
+			this.UserRole = userRole;
+			this.UserRoleLevel = userRoleLevel;
+		}
+		public string UserName;
+		public string LoginName;
+		public string UserRole;
+		public int UserRoleLevel;
+	}
+}
