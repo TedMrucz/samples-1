9cc10b44495ec79c717e5219d2b1bccb0280de93
TedMruczkowski <TedMruczkowski@users.noreply.github.com>
2017-07-21 19:49:24 -0400

    Add files via upload

---
+++ SR/UnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using CallCentre.Data;
+using WebHost.WebAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+			//string connection = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=CallCentre;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			//using (var context = new DataContext(null))
+			//{
+			//	var service = new CallerCentreController(context);
+			//	var list = service.GetMessages();
+
+			//	Assert.AreEqual(1, list.Count);
+			//	//Assert.AreNotEqual(null, committiess[0].CommitteeType);
+			//	//Assert.AreNotEqual(null, committiess[0].CommitteeMembers);
+			//}
+		}
+    }
+}
